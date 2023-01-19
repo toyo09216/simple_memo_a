6080c5e85cd72b283f26e65e3c72d7c2dc6cb395
toyo09216 <atsushi09216@gmail.com>
2023-01-19 17:05:34 +0900

    モデルとマイグレーションファイルの作成

---
+++ database/migrations/2023_01_19_165943_create_memos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            // BigInteger桁数の大きい数値型のカラム、unsigned（+-の）符号なし、符号なしにしておかないと外部キー制約が使えない。第二引数にtrueを入れておくと、Auto Incrementをしれくれる。
+            $table->unsignedBigInteger('id', true);
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            // 論理削除を定義→deleted_atを自動生成
+            $table->softDeletes();
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接記載。
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            // 外部キー制約、usersテーブルにあるidのものだけを、user_idに指定できる。
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
