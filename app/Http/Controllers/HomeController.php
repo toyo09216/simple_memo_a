fa79e503a85fe22946119e9ee667d0b75271ffc9
toyo09216 <atsushi09216@gmail.com>
2023-01-20 18:48:27 +0900

    メモの編集機能

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,4 +43,26 @@ class HomeController extends Controller
         
         return redirect(route('home'));
     }
+
+    public function edit($id)
+    {
+        // ここでメモを取得
+        $memos = Memo::select('memos.*') // memosテーブルの全てを選択
+            ->where('user_id', '=', \Auth::id()) //現在ログイン中のユーザーのものを
+            ->whereNull('deleted_at') //deleted_at がnullのものを
+            ->orderBy('updated_at', 'DESC') //ASC=昇順、DESC=降順
+            ->get(); //取得
+
+        $edit_memo = Memo::find($id);
+
+        return view('edit', compact('memos', 'edit_memo')); //view側に値を渡す
+    }
+
+    public function update(Request $request)
+    {
+        $posts = $request->all();
+        Memo::where('id', $posts['memo_id'])->update(['content'=> $posts['content']]);
+        
+        return redirect(route('home'));
+    }
 }
